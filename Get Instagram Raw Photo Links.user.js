ddea54d6e0ce83def1da7c90336e4dc65f4b52e2
翁君牧 <chunmu@livemail.tw>
2018-10-10 12:05:53 +0800

    Add files via upload

--- Get Instagram Raw Photo Links.user.js
+++ Get Instagram Raw Photo Links.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Get Instagram Raw Photo Links
 // @namespace    http://kevinweng.tk/
-// @version      1.1
+// @version      1.2
 // @description  Get Instagram Raw Photo Links
 // @author       nevikw39
 // @match        https://www.instagram.com/*
 // @grant        GM_setClipboard
+// @grant        GM_notification
+// @grant        GM_openInTab
 // @downloadURL  https://github.com/nevikw39/Get-Instagram-Raw-Photo-Links/raw/master/Get%20Instagram%20Raw%20Photo%20Links.user.js
 // @updateURL  https://github.com/nevikw39/Get-Instagram-Raw-Photo-Links/raw/master/Get%20Instagram%20Raw%20Photo%20Links.user.js
 // ==/UserScript==
@@ -14,8 +16,11 @@
     'use strict';
     document.addEventListener('click', (e) => {
         if(e.target.className=="_9AhH0") {
-            GM_setClipboard (e.target.previousSibling.childNodes[0].getAttribute("src"));
-            alert("Linked Cpoied : "+e.target.previousSibling.childNodes[0].getAttribute("src"));
+            var url = e.target.previousSibling.childNodes[0].getAttribute("src");
+            GM_setClipboard (url);
+            GM_notification("The following link has been copied : " + url, "Link is Copied :))", url, function() {
+                GM_openInTab(url,false);
+            });
         }
     })
     document.getElementBy
